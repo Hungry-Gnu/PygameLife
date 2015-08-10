a1c18afce9f965c6bf6691cb4b6851924c4b08a8
luke-nukem <garagearcades@gmail.com>
2015-08-10 17:12:04 +1200

    More modification to menu system

--- game.py
+++ game.py
@@ -14,6 +14,7 @@ GREY     = ( 50,   50,  50)
 class LifeGame:
     def __init__(self):
         self.firstStart = 1
+        self.enteredMainMenu = 1
         pygame.init()
         #initSize = [1920,1000]
         initSize = [1024,768]
@@ -70,6 +71,30 @@ class LifeGame:
             text = self.font.render(self.instr[i], 1, WHITE)
             x = self.menuSurf.get_rect().centerx - text.get_rect().centerx
             self.menuSurf.blit(text,(x,100+i*40))
+    
+    def mainMenu(self):
+        if self.enteredMainMenu == 1:
+            self.menuLife.resetGrid()
+            for pos in [(2, 0), (1, 0), (1, 1), (1, 2), (0, 1)]:
+                x = int(len(self.menuLife.grid[0])*0.2) + pos[0]
+                y = int(len(self.menuLife.grid)*0.4) + pos[1]
+                self.menuLife.grid[y][x] = 1
+                self.menuLife.alive.append((x,y))
+            # Cross
+            for pos in [(1, 0), (1, 1), (1, 2), (2, 1), (0, 1)]:
+                x = int(len(self.menuLife.grid[0])*0.5) + pos[0]
+                y = int(len(self.menuLife.grid)*0.8) + pos[1]
+                self.menuLife.grid[y][x] = 1
+                self.menuLife.alive.append((x,y))
+                self.menuLife.grid[y][x+4] = 1
+                self.menuLife.alive.append((x+4,y))
+            # Tetrinomo
+            for pos in [(1, 0), (1, 1), (1, 2), (0, 2), (2, 1)]:
+                x = int(len(self.menuLife.grid[0])*0.8) + pos[0]
+                y = int(len(self.menuLife.grid)*0.2) + pos[1]
+                self.menuLife.grid[y][x] = 1
+                self.menuLife.alive.append((x,y))
+            self.enteredMainMenu = 0
             
     def printPaused(self):
         self.menuSurf.fill(BLACK)
@@ -94,43 +119,46 @@ class LifeGame:
             self.getEvents()
             # Menu State
             if self.state == 1:
-                self.menuSurf.fill(BLACK)
-                if self.firstStart == 1 and framecount > 3:
-                    self.screen.fill(BLACK)
-                    self.menuLife.drawBG()
-                    self.menuLife.update()
-                    framecount = 0
-                elif self.firstStart == 0:
-                    self.screen.fill(BLACK)
-                    self.cellLife.paused()
-                self.printHelp()
-                self.screen.blit(self.menuSurf,(0,0))
+                    self.menuSurf.fill(BLACK)
+                    if self.firstStart == 1 and framecount > 3:
+                        self.screen.fill(BLACK)
+                        self.menuLife.drawBG()
+                        self.menuLife.update()
+                        self.printHelp()
+                        framecount = 0
+                    elif self.firstStart == 0:
+                        self.mainMenu()
+                        self.printHelp()
+                        self.screen.fill(BLACK)
+                        self.menuLife.drawBG()
+                        self.menuLife.update()
+                        #self.cellLife.paused()
+                    self.screen.blit(self.menuSurf,(0,0))
             # Paused State    
             elif self.state == 2:
-                self.screen.fill(BLACK)
-                if self.firstStart == 0:
-                    self.printPaused()
-                else:
-                    self.menuSurf.fill(BLACK)
-                self.cellLife.paused()
-                self.screen.blit(self.menuSurf,(0,0))
+                    self.screen.fill(BLACK)
+                    if self.firstStart == 0:
+                        self.printPaused()
+                    else:
+                        self.menuSurf.fill(BLACK)
+                    self.cellLife.paused()
+                    self.screen.blit(self.menuSurf,(0,0))
             # Running State
             elif self.state == 3:
-                if framecount > self.speed:
-                    self.screen.fill(BLACK)
-                    self.cellLife.drawBG()
-                    self.cellLife.update()
-                    framecount = 0
-                if msgTime > 60:
-                    self.menuSurf.fill(BLACK)
-                    self.message = []
-                    self.screen.blit(self.menuSurf,(0,0))
-                    msgTime = 0
-                else:
-                    self.printMsg()
-                    self.screen.blit(self.menuSurf,(0,0))
-                    msgTime +=1
-                    
+                    if framecount > self.speed:
+                        self.screen.fill(BLACK)
+                        self.cellLife.drawBG()
+                        self.cellLife.update()
+                        framecount = 0
+                    if msgTime > 60:
+                        self.menuSurf.fill(BLACK)
+                        self.message = []
+                        self.screen.blit(self.menuSurf,(0,0))
+                        msgTime = 0
+                    else:
+                        self.printMsg()
+                        self.screen.blit(self.menuSurf,(0,0))
+                        msgTime +=1
             # Game Tick
             framecount += 1
             self.clock.tick(60)
@@ -158,8 +186,12 @@ class LifeGame:
                 if pygame.key.get_pressed()[pygame.K_RETURN] == self.state == 1:
                     self.state = 2
                 if pygame.key.get_pressed()[pygame.K_ESCAPE] == 1:
-                    if   self.state == 3: self.state = 1
-                    elif self.state == 1: self.state = 3
+                    if   self.state == 3:
+                        self.state = 1
+                        self.enteredMainMenu = 1
+                    elif self.state == 1:
+                        self.state = 3
+                        self.enteredMainMenu = 0
                 if pygame.key.get_pressed()[pygame.K_p] == 1:
                     self.firstStart = 0
                     if   self.state == 2: self.state = 3
